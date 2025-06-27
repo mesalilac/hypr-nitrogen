e1fcffd7bd69bc6e31f3cae8f9fe7254ccacd666
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-06-27 11:33:38 +0300

    enable `eqeqeq`

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default defineConfig([
     eslintConfigPrettier,
     {
         rules: {
+            eqeqeq: 'error',
             'no-console': [
                 'error',
                 {
