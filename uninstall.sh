30012e8ab7307b1157a005aec9b1f9c0cd12ffca
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-05 00:58:28 +0300

    remove app data and cache

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,10 @@
 set -e
 
 echo "Removeing hypr-nitrogen.AppImage"
-
 rm --verbose ~/.local/bin/hypr-nitrogen
+
+echo "Removing app data directory"
+rm -rfv ~/.local/share/hypr-nitrogen/
+
+echo "Removing cache directory"
+rm -rfv ~/.cache/hypr-nitrogen/
