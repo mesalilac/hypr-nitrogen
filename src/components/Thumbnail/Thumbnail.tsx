388dd93e78c3f495c0257fa6f87278cd185381cf
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-04 05:06:04 +0300

    only load `Thumbnail` when in view

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,6 +1,7 @@
 import { convertFileSrc } from '@tauri-apps/api/core';
 import fallbackImage from '@assets/fallback-image.svg';
 import * as ipc from '@ipc';
+import { createVisibilityObserver } from '@solid-primitives/intersection-observer';
 
 interface Props {
     wallpaper: ipc.types.Wallpaper;
@@ -10,13 +11,24 @@ interface Props {
 }
 
 function Thumbnail(props: Props) {
+    let imgRef: HTMLImageElement | undefined;
+
+    const useVisibilityObserver = createVisibilityObserver({ threshold: 0.2 });
+
+    const visible = useVisibilityObserver(() => imgRef);
+
     return (
         <div onClick={props.onClick}>
             <img
                 id={props.is_selected ? 'thumbnail-selected' : ''}
                 class={`thumbnail ${props.is_active ? 'thumbnail-active' : ''}`}
-                src={convertFileSrc(props.wallpaper.thumbnail_path)}
+                src={
+                    visible()
+                        ? convertFileSrc(props.wallpaper.thumbnail_path)
+                        : undefined
+                }
                 loading='lazy'
+                ref={imgRef}
                 onError={(e) => (e.currentTarget.src = fallbackImage)}
             />
         </div>
