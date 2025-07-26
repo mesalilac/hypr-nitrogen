971998fea19d00d58da03afd3dab596027f7d76e
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-26 21:08:55 +0300

    feat(frontend): Display `is_favorite` state

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -2,6 +2,8 @@ import fallbackImage from '@assets/fallback-image.svg';
 import * as ipc from '@ipc';
 import { createVisibilityObserver } from '@solid-primitives/intersection-observer';
 import { convertFileSrc } from '@tauri-apps/api/core';
+import { createSignal } from 'solid-js';
+import toast from 'solid-toast';
 
 interface Props {
     wallpaper: ipc.types.Wallpaper;
@@ -11,26 +13,44 @@ interface Props {
 }
 
 export function Thumbnail(props: Props) {
+    const [favorite, setFavorite] = createSignal(props.wallpaper.is_favorite);
     let imgRef: HTMLImageElement | undefined;
 
     const useVisibilityObserver = createVisibilityObserver({ threshold: 0.2 });
 
     const visible = useVisibilityObserver(() => imgRef);
 
+    function handleFavoriteClick() {
+        ipc.update
+            .wallpaper_favorite({
+                id: props.wallpaper.id,
+                newValue: !favorite(),
+            })
+            .then((res) => {
+                setFavorite(res.data.is_favorite);
+            })
+            .catch((e) => toast.error(e));
+    }
+
     return (
-        <img
-            id={props.is_selected ? 'thumbnail-selected' : ''}
-            class={`thumbnail ${props.is_active ? 'thumbnail-active' : ''}`}
-            src={
-                visible()
-                    ? convertFileSrc(props.wallpaper.thumbnail_path)
-                    : undefined
-            }
-            onClick={props.onClick}
-            loading='lazy'
-            ref={imgRef}
-            onError={(e) => (e.currentTarget.src = fallbackImage)}
-            title={props.wallpaper.keywords}
-        />
+        <div class='thumbnail'>
+            <img
+                id={props.is_selected ? 'thumbnail-img-selected' : ''}
+                class={`thumbnail-img ${props.is_active ? 'thumbnail-img-active' : ''}`}
+                src={
+                    visible()
+                        ? convertFileSrc(props.wallpaper.thumbnail_path)
+                        : undefined
+                }
+                onClick={props.onClick}
+                loading='lazy'
+                ref={imgRef}
+                onError={(e) => (e.currentTarget.src = fallbackImage)}
+                title={props.wallpaper.keywords}
+            />
+            <button class='thumbnail-favorite' onClick={handleFavoriteClick}>
+                {favorite() ? 'Unfavorite' : 'Favorite'}
+            </button>
+        </div>
     );
 }
